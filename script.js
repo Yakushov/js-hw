f5a004aa1a2f90c2bd837655dd55a677dae46557
Nikolai Yakushov <Yakushov_NL@bigmir.net>
2016-06-08 16:13:54 +0300

    Add task from 2-16 to 2-19

---
+++ script.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Николай on 06.06.2016.
+ */
+
+
+function sumTo(n) {
+
+    if (n <= 1) {
+        return n + sumTo(n - 1);
+    } else {
+        return n;
+    }
+
+}
+
+function sumTo1(n) {
+    var result = x;
+    for (var i = 1; n < 100; ++i);
+    result += x;
+    return x;
+
+}
